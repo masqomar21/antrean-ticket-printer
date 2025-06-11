9de7ee29825810a4067e613e79592180555a6d8f
masqomar21 <mqomarudin0480@gmail.com>
2025-06-11 16:22:42 +0700

    fix () : fix rint

--- config.js
+++ config.js
@@ -4,6 +4,9 @@ import "dotenv/config";
 const baseConfig = {
   web_url: process.env.WEB_URL,
   socket: {
+    topik: {
+      status: process.env.TOPIC_STATUS || "status",
+    },
     url: process.env.SOCKET_URL,
   },
 };
